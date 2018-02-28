72be59e9d1190f947c308c67815406338a5ba62e
Anton <anton.bil@hz.nl>
2018-02-28 13:17:26 +0100

    added notifications

--- src/AlbumList.js
+++ src/AlbumList.js
@@ -3,8 +3,9 @@ import Modal from 'react-modal';
 import ReactScrollbar from 'react-scrollbar-js';
 import CommonList from './CommonList';
 import {ContextMenu2} from './ContextMenu.js';
-import{getImagePath}from './Utils.js';
+import{getImagePath,getDimensions}from './Utils.js';
 import Img from 'react-image';
+import { ToastContainer, toast } from 'react-toastify';
 
 function makeFileListElement(content){
 
@@ -15,7 +16,6 @@ function makeFileListElement(content){
     } catch(e){}
     if(typeof content.getMetadata().directory !== 'undefined'){
         item.MPD_file_path_name=content.getPath();
-        //console.log(content.getMetadata());
     }
     else{
         item.MPD_file_title=content.getDisplayName();
@@ -28,11 +28,26 @@ function makeFileListElement(content){
     return item;
 }
 
+let notifyMessage = function (message) {
+    let mytoastId = null;
+    let notify = () => {
+        mytoastId = toast(message, {
+            position: toast.POSITION.BOTTOM_CENTER, autoClose: 4000, closeButton: false
+        });
+    };
+    notify();
+    setTimeout(function () {
+        toast.dismiss(mytoastId);
+
+    }, 4500);
+};
+
 class PopupAlbum extends React.Component {
     constructor(props) {
         super(props);
         this.closeModal=props.closeModal;
         this.album=props.album;
+        this.itemChosen=false;
         this.state = {
             items: []
         };
@@ -53,14 +68,19 @@ class PopupAlbum extends React.Component {
         });
     }
 
-    render(){
-        let width = window.innerWidth
-            || document.documentElement.clientWidth
-            || document.body.clientWidth;
+    handleItemClick(e,i) {
+        e.preventDefault();
+        this.itemChosen=true;
+        let path=this.state.items[i].MPD_file_file;
+        let message="add file:"+path;
+        try {
+            notifyMessage(message);
+        } catch(e){}
+        mpd_client.addSongToQueueByFile(path);
+    }
 
-        let height = window.innerHeight
-            || document.documentElement.clientHeight
-            || document.body.clientHeight;
+    render(){
+        let {width, height} = getDimensions();
         let myScrollbar = {
             margin:10,
             width: width-50,
@@ -77,23 +97,26 @@ class PopupAlbum extends React.Component {
             }
         };
         return (
-            <Modal onClick={this.closeModal}
+            <Modal
                    isOpen={true}
                    ariaHideApp={false}
 
                    onRequestClose={this.closeModal}
-                   style={customStyles}
+                   
                    contentLabel="Album Info"
             >
 
                 <ReactScrollbar style={myScrollbar}>
-                    <div  className="popup"  onClick={this.closeModal}>
+                    <div  className="popup"  onClick={(e) => {if (!this.itemChosen)this.closeModal(e);this.itemChosen=false;}}>
+                        <ToastContainer autoClose={2000} />
                         <Img src={getImagePath("/" + this.album)}
-                             className="list-image-large"/>
+                             className="list-image-large"/>{this.album}
                         <ul>
                             {this.state.items.map((listValue, i) => {
 
-                                return <div  className="list-item" ><li key={i} style={this.listStyle}>
+                                return <div  className="list-item" onClick={(e) => {
+                                    this.handleItemClick(e,i);
+                                }}><li key={i} style={this.listStyle}>
                                     {listValue.MPD_file_title}</li></div>;
                             })}
                         </ul>
@@ -126,7 +149,6 @@ class AlbumList extends CommonList {
         mpd_client.getDirectoryContents(dir, (directory_contents) => {
             let myTotalList = [];
             let mylist = [];
-            //console.log(directory_contents);
             directory_contents.forEach((content) => {
                 let element = makeFileListElement(content);
                 try {
@@ -134,20 +156,20 @@ class AlbumList extends CommonList {
                     let path = element.MPD_file_path_name;
                     path = path.substr(path.lastIndexOf('/') + 1);
                     mylist = mylist.concat(path)
-                    //console.log(element);
                 } catch (e) {
                 }
             });
-            this.totalList = myTotalList;
+
             if (mylist.length > 0) {
+                this.totalList = myTotalList;
                 this.prevdirs = this.prevdirs.concat(dir);
                 this.setState(previousState => ({
                     items: mylist, showPopup: false
                 }));
             } else {
                 //let  path=this.getFilePath(this.selection);
-                //console.log("add dir:",dir);
                 mpd_client.addSongToQueueByFile(dir);
+                notifyMessage("add dir:"+dir);
                 //no items, display context menu
             }
         })
@@ -162,7 +184,6 @@ class AlbumList extends CommonList {
     }
 
     handleClick(index) {
-        //console.log("clicked:",index);
         if (!this.albumsContextmenu.state.visible) {
             let path = this.getFilePath(index);
             this.selection = index;
@@ -172,11 +193,7 @@ class AlbumList extends CommonList {
     };
 
     contextResult(choice) {
-        //console.log("albums choice:",choice);
-        //console.log("action with:",albumList.selection);
-        console.log("this.selection:", this.selection);
         let path = this.getFilePath(this.selection);
-        console.log("path:", path);
 
         if (choice === "Add") {
             mpd_client.addSongToQueueByFile(path);
@@ -202,7 +219,7 @@ class AlbumList extends CommonList {
     }
 
 
-    closeModal() {
+    closeModal(e) {
         this.setState({
             items: this.state.items,
             modalIsOpen: false
@@ -218,24 +235,18 @@ class AlbumList extends CommonList {
 
     backClick() {//this.prevdirs=this.prevdirs.splice(-1,1);
         if (this.prevdirs.length > 1) {
-            //console.log("back1",this.prevdirs);
-            //this.prevdirs=this.prevdirs.splice(-1,1);
             this.prevdirs.pop();
-            //console.log("back2",this.prevdirs);
             let dir = this.prevdirs[this.prevdirs.length - 1];
-            //console.log("back3",dir);
-            //this.prevdirs=this.prevdirs.splice(-1,1);
             this.prevdirs.pop();
             this.getDirectoryContents(dir);
         }
-        //console.log("back");
     }
 
 
     render() {
         return (
             <div><ContextMenu2 onRef={ref => (this.albumsContextmenu = ref)}/><br/>
-                <button onClick={this.backClick}>Back</button>
+                <button onClick={this.backClick}>Back</button><ToastContainer />
                 <ul>
                     {this.state.items.map((listValue, i) => {
                         let path = getImagePath("/" + this.totalList[i].mpd_file_path);
--- src/Utils.js
+++ src/Utils.js
@@ -9,4 +9,15 @@ function getTime(n){
 function getImagePath(path){
     return "http://192.168.2.8:8081/FamilyMusic"+path+"/folder.jpg"
 }
-export{padDigits,getTime,getImagePath}
+let getDimensions = function () {
+    let width = window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+
+    let height = window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
+    return {width, height};
+};
+
+export{padDigits,getTime,getImagePath,getDimensions}
