f7d8d86dfebaf64daab9f90309cba6de499737b6
Anton <anton.bil@hz.nl>
2018-03-04 10:59:19 +0100

    added server

--- src/SelectServer.js
+++ src/SelectServer.js
@@ -7,6 +7,7 @@ class SelectServer extends SelectOption {
         this.submitMessage="Select host";
         this.options=[
             {name:'keuken',value:true, host:"http://192.168.2.16/mpdjs"},
+            {name:'kamer',value:false, host:"http://192.168.2.9/mpdjs"},
             {name:'wifiberry',value:false, host:"http://192.168.2.12/mpdjs"}];
     }
 
